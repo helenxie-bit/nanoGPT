e0ec3654562bac0a0d60c8c480c4fdda15c6563d
Hezhi Xie <helen@hezhis-air.attlocal.net>
2024-05-13 21:51:35 -0700

    update

--- model.py
+++ model.py
@@ -103,12 +103,12 @@ class SlidingWindowAttention(nn.Module):
         if not self.flash:
             print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
             # sliding window mask to ensure that attention is only applied to the fixed context window
-            mask = torch.zeros(config.block_size, config.block_size)
+            self.mask = torch.zeros(config.block_size, config.block_size)
             for i in range(config.block_size):
-                mask[i, max(0, i - config.window_size):min(config.block_size, i + config.window_size + 1)] = 1
+                self.mask[i, max(0, i - config.window_size):min(config.block_size, i + config.window_size + 1)] = 1
                 if self.n_regist > 0:
-                    mask[i, :self.n_regist] = 1
-            self.register_buffer("bias", mask.view(1, 1, config.block_size, config.block_size))
+                    self.mask[i, :self.n_regist] = 1
+            self.register_buffer("bias", self.mask.view(1, 1, config.block_size, config.block_size))
         self.custom_softmax = config.custom_softmax
 
     def forward(self, x):
@@ -127,7 +127,7 @@ class SlidingWindowAttention(nn.Module):
         else:
             # manual implementation of attention
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            att = att.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))
             att = CustomSoftmax()(att) if self.custom_softmax else F.softmax(att, dim=-1)
             att = self.attn_dropout(att)
             y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
