e18342f97e01be299da6de89f85877469907a104
Hezhi Xie <helen@hezhis-air.attlocal.net>
2024-05-17 17:38:48 -0700

    update

--- model.py
+++ model.py
@@ -272,9 +272,10 @@ class GPT(nn.Module):
         
         # add register tokens
         if self.config.n_regist > 0:
-            x = self.transformer.drop(tok_emb + pos_emb[:, self.config.n_regist:, :])
+            print(tok_emb.shape, pos_emb.shape)
+            x = self.transformer.drop(tok_emb + pos_emb[self.config.n_regist:, :])
             register_tokens = nn.Parameter(torch.zeros(1, self.config.n_regist, self.config.n_embd))
-            register_tokens = register_tokens + pos_emb[:, :self.config.n_regist, :]
+            register_tokens = register_tokens + pos_emb[:self.config.n_regist, :]
             register_tokens = register_tokens.expand(b, -1, -1)
             x = torch.cat((register_tokens, x), dim=1)
         else:
