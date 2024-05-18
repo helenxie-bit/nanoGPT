9bd8d190ad75ee9613e4e795c22bf4034e42702a
Hezhi Xie <helen@hezhis-air.attlocal.net>
2024-05-17 18:00:04 -0700

    update

--- model.py
+++ model.py
@@ -288,8 +288,9 @@ class GPT(nn.Module):
             # if we are given some desired targets also calculate the loss
             logits = self.lm_head(x)
             if self.config.n_regist > 0:
-                logits = logits[:, self.config.n_regist:, :]
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+                loss = F.cross_entropy(logits[:, self.config.n_regist:].view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+            else:
+                loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
             logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
